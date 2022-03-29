e34fea95b3a952cdd09d4c135352a59784f0c075
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-29 15:18:47 +0300

    make Product component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
 import products from "../products";
 
 function HomeScreen() {
@@ -8,7 +9,7 @@ function HomeScreen() {
             <Row>
                 { products.map(product =>(
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <h3>{ product.name }</h3>
+                        <Product product={ product }/>
                     </Col>
                 ))}
             </Row>
