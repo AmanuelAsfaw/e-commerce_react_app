3b645b468a8a43f6afde82eda6f0139bd1c998d3
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-29 16:25:40 +0300

    make product detail screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen/>}/>
+            <Route path='/product/:id' element={<ProductScreen/>}/>
           </Routes>
         </Container>        
       </main>
