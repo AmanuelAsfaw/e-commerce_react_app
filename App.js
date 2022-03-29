2380379e0b794f59db7551e071cd27aa937d7ceb
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-29 16:51:36 +0300

    Update file str

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen/>}/>
+            <Route path='/product/:id' element={<ProductScreen/>}/>
+          </Routes>
+        </Container>        
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
