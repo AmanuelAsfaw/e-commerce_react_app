5f59709ae59dc2bc42b107cae1f6f2e783d562bb
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-03-31 11:16:46 +0300

    Make product action-reducer

--- src/store.js
+++ src/store.js
@@ -2,7 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducer = combineReducers({});
+import { productListReducers } from './reducers/productReducers'
+
+const reducer = combineReducers({
+    productList : productListReducers,
+});
 
 const initialState = {}
 
