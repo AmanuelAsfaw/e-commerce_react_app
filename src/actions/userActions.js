b9d33b3e2cab9119d008126c06cc46b1995b45cf
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-04-08 14:09:25 +0300

    Add login-dispatch after register-success

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -53,7 +53,7 @@ export const registerAction = (name, email, password) => async (dispatch) => {
         }
 
         const { data } = await axios.post(
-            'http://127.0.0.1:8000' + '/api/users/register/',
+            API_URL + '/api/users/register/',
             {
                 'name': name,
                 'email': email,
