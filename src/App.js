8693a9d2dae25d58fed42dfcb94bd5962f94ee67
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-04-14 16:53:48 +0300

    Make PaymentScreen

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import PaymentScreen from './screens/PaymentScreen';
 import ProductScreen from './screens/ProductScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
@@ -25,6 +26,7 @@ function App() {
             <Route path='/register' element={<RegisterScreen/>}/>
             <Route path='/profile' element={<ProfileScreen/>}/>
             <Route path='/shipping' element={<ShippingScreen/>}/>
+            <Route path='/payment' element={<PaymentScreen/>}/>
             <Route path="/cart">
               <Route index element={<CartScreen />} />
               <Route path=":id" element={<CartScreen />} />
--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -27,7 +27,7 @@ function ShippingScreen() {
     
     return (
         <FormContainer>
-            <CheckoutSteps step1 step2/>
+            <CheckoutSteps step1 step2 step3/>
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <FormGroup controlId="address">
